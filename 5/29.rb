c3c80333d58b25f738c8b7beb88d7f63b5128970
umekishimaru <riki12344649@gmail.com>
2024-05-29 19:14:49 +0900

    四則演算演習完了

---
+++ 5/29.rb
@@ -0,0 +1,17 @@
+#四則計算
+num_1 = 10
+num_2 = 3
+num_3 = 2
+num_4 = 10_000
+
+puts num_1 + num_2
+puts num_1 - num_2
+puts num_1 * num_2
+puts num_4 / num_1
+puts num_1 / num_2
+puts num_1 % num_2
+puts num_3 ** num_3
+puts num_3 ** num_2
+# 2 ** 2
+# 2 ** 3
+
